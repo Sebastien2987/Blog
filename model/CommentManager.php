fa1c0dd6b94cf07df269ab308e6c2f7784a8defa
Sébastien <33893937+Sebastien2987@users.noreply.github.com>
2017-12-27 13:38:50 +0100

    Add files via upload

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -7,7 +7,7 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_order FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_order, report FROM comments WHERE post_id = ?  ORDER BY comment_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
@@ -16,9 +16,19 @@ class CommentManager extends Manager
     public function postComment($postId, $author, $comment)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, report, comment_date) VALUES(?, ?, ?, 0, NOW())');
         $affectedLines = $comments->execute(array($postId, $author, $comment));
 
         return $affectedLines;
+    }
+
+        public function editReport()
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE comments SET report = "1" WHERE id = :id');
+        $affectedreport = $req->execute(array(
+            'id' => $_GET['id']));
+
+        return $affectedreport;
     }
 }
