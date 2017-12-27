c3b2594674bf76389c31d297138adcf375c023dc
Sébastien <33893937+Sebastien2987@users.noreply.github.com>
2017-12-27 13:39:20 +0100

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -7,17 +7,29 @@ try {
             adminaddPost();
         }
 
-    elseif ($_GET['action'] == 'addPost') {
-        if (!empty($_POST['title']) && !empty($_POST['content'])) {
+        elseif ($_GET['action'] == 'addPost') {
+            if (!empty($_POST['title']) && !empty($_POST['content'])) {
                 addPost($_POST['title'], $_POST['content']);
             }
-            else {
+             else {
             // Autre exception
              throw new Exception('Tous les champs ne sont pas remplis !');
             }
         }
         elseif ($_GET['action'] == 'listPosts') {
             listPosts();
+        }
+        elseif ($_GET['action'] == 'listModerations') {
+            listModerations();
+        }
+        elseif ($_GET['action'] == 'suppComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                suppComment();
+            }
+            else {
+                // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
+                throw new Exception('Aucun commentaire sélectionné');
+            }
         }
         elseif ($_GET['action'] == 'suppPost') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
@@ -31,10 +43,20 @@ try {
         elseif ($_GET['action'] == 'listPostsedit') {
             listPostsedit();
         }
+
+        elseif ($_GET['action'] == 'selecteditPost') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                    selecteditPost($_GET['id']);
+                }
+                else {
+                    // Autre exception
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
         elseif ($_GET['action'] == 'editPost') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
                 if (!empty($_POST['title']) && !empty($_POST['content'])) {
-                    editPost($_GET['id'], $_POST['content'], $_POST['content']);
+                    editPost($_POST['title'], $_POST['content']);
                 }
                 else {
                     // Autre exception
